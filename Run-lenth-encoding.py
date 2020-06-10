b7bb3b68f1dfd1111fcfcc02ab513ff351aee047
userpy <dima6225@yandex.ru>
2020-06-10 17:39:03 +0300

    add context manager

--- Run-lenth-encoding.py
+++ Run-lenth-encoding.py
@@ -60,21 +60,52 @@ def decode(input, output):
     output.close()
 
 
+
+
+class FileReader:
+    def __init__(self, input, output):
+        self.input = input
+        self.output = output
+        self.RleEncode = RleEncode
+        self.RleDecode = RleDecode
+
+    def __enter__(self):
+        self.input = open(self.input, 'r')
+        self.output = open(self.output, 'w')
+        return self
+
+    def encode(self):
+        for i in self.input.readlines():
+            val = self.RleEncode(i)
+            self.output.write(val.rle_encode())
+
+    def decode(self):
+        for i in self.input.readlines():
+            val = self.RleDecode(i)
+            self.output.write(val.rle_decode())
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.input.close()
+        self.output.close()
+
+
 def main():
-
-
     parser = argparse.ArgumentParser(description='decoder')
-
     parser.add_argument('-i', '--input', default=None)
     parser.add_argument('-o', '--output', default=None)
     parser.add_argument('-e', '--encode', type=bool, nargs='?', const=True, default=False)
     parser.add_argument('-d', '--decode', type=bool, nargs='?', const=True, default=False)
-
     args = parser.parse_args()
+
     if args.encode:
-        encode(output=args.output, input=args.input)
+        with FileReader(output=args.output, input=args.input) as f:
+            f.encode()
+
+
     elif args.decode:
-        decode(output=args.output, input=args.input)
+        with FileReader(output=args.output, input=args.input) as f:
+            f.decode()
 
 
 
